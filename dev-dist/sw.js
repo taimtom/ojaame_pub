6d8a6e3fef7daafe795d028fdd581df7e1a64563
Theophilus Olaezekiel <theophilus.ola-ezekiel@innova.vet>
2026-05-18 23:45:30 +0100

    update

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-bbbdf4d3'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/index.html",
-    "revision": "0.gpn9li7fbe"
+    "revision": "0.vlun5889es"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
